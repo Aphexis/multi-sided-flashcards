adcb0264fa1b6709d8382fe12f1819c5e35f42e9
Steph X <stephxca@gmail.com>
2019-08-10 00:58:30 -0400

    add named range in build_set()

--- spreadsheet.py
+++ spreadsheet.py
@@ -39,21 +39,61 @@ def get_sides(set_id):
     print(values)
 
 def create_set(card_info, side_info):  # params are 2D arrays to be appended to the spreadsheet
+    ### Define a named range based on data
+    ## Get the current size of the spreadsheet
+    cards_result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='Cards').execute() # TO-DO: use a batch get
+    cards_values = cards_result.get('values', [])
+    sides_result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='Sides').execute()
+    sides_values = sides_result.get('values', [])
+    requests = [
+        {
+            "addNamedRange": {
+                "namedRange": {
+                    "name": card_info[0][0] + "_Cards",
+                    "range": {
+                        "sheetId": 1919301030,
+                        "startRowIndex": len(cards_values),
+                        "endRowIndex": len(card_info) + len(cards_values),
+                        "startColumnIndex": 0,
+                        "endColumnIndex": len(card_info[1]),
+                    },
+                }
+            }
+        },
+        {
+            "addNamedRange": {
+                "namedRange": {
+                    "name": card_info[0][0] + "_Sides",
+                    "range": {
+                        "sheetId": 462820485,
+                        "startRowIndex": len(sides_values),
+                        "endRowIndex": len(side_info) + len(sides_values),
+                        "startColumnIndex": 0,
+                        "endColumnIndex": len(side_info[1]),
+                    },
+                }
+            }
+        }
+    ]
+    body = {'requests': requests}
+    response = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id,body=body).execute()
+
+    # https://developers.google.com/sheets/api/samples/ranges
+
+    ### Add the flashcard data
     body_card = {
         'values': card_info
     }
     body_side = {
         'values': side_info
     }
-    result_card = service.spreadsheets().values().append(
+    result_card = service.spreadsheets().values().append(  # TO-DO: replace with batch append?
         spreadsheetId=spreadsheet_id, range='Cards',
         valueInputOption='USER_ENTERED', body=body_card).execute()
     result_side = service.spreadsheets().values().append(
         spreadsheetId=spreadsheet_id, range='Sides',
         valueInputOption='USER_ENTERED', body=body_side).execute()
 
-    # TO-DO: define a named range based on data
-    # https://developers.google.com/sheets/api/samples/ranges
 
 def build_set(set_name,card_text,side_names): # params are the info given when a set is created
     #build the side_info array
