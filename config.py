e6100e19e7d382e0c26e4a3c8c2c93baa2b1ae94
Aphexis <stephxca@gmail.com>
2019-12-30 01:27:47 -0500

    converted from Google Sheets to MySQL database

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-DEBUG = True
-DEVELOPMENT = True
+import os
+
+
+class Config:
+    DEBUG = True
+    DEVELOPMENT = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root!$23@localhost/flashcards'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ models.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Set_SQL(Base):
+    __tablename__ = 'sets'
+
+    set_id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(255))
+    description = Column(String(500))
+
+
+class Card_SQL(Base):
+    __tablename__ = 'cards'
+
+    card_ID = Column(INTEGER(11), primary_key=True)
+    set_id = Column(ForeignKey('sets.set_id'), nullable=False, index=True)
+    card_order = Column(INTEGER(11))
+
+    set = relationship('Set_SQL')
+
+
+class Side_SQL(Base):
+    __tablename__ = 'sides'
+
+    side_id = Column(INTEGER(11), primary_key=True)
+    set_id = Column(ForeignKey('sets.set_id'), nullable=False, index=True)
+    name = Column(String(255))
+    side_order = Column(INTEGER(11))
+
+    set = relationship('Set_SQL')
+
+
+class Cell_SQL(Base):
+    __tablename__ = 'cells'
+
+    cell_id = Column(INTEGER(11), primary_key=True)
+    card_id = Column(ForeignKey('cards.card_ID'), nullable=False, index=True)
+    side_id = Column(ForeignKey('sides.side_id'), nullable=False, index=True)
+    info = Column(String(1000))
+
+    card = relationship('Card_SQL')
+    side = relationship('Side_SQL')
