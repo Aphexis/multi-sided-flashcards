68c1229a7a2c7ae3501e5bffbd4757127820b664
Aphexis <stephxca@gmail.com>
2020-01-12 00:36:15 -0500

    add dynamic form fields

--- static/javascript/create.js
+++ static/javascript/create.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
     // $('#form').submit(function(e){
     //     var memberArr = [];
@@ -31,8 +29,52 @@ $(document).ready(function(){
     //         '            <td class="tg-0lax">def</td>\n' +
     //         '            <td class="tg-0lax">ghi</td>';
     // });
-    $('button').click(function(){
-        // Add new form fields
-        $('#dynamic_form').append('<span class="member_data"><input type="text" name="name"><input type="text" name="date"></span><br>');
-    });
+    // $('button').click(function(){
+    //     // Add new form fields
+    //     $('#dynamic_form').append('<span class="member_data"><input type="text" name="name"><input type="text" name="date"></span><br>');
+    // });
+
+
 });
+
+
+function addRow(tableID) {
+    let tableRef = document.getElementById(tableID);
+    let rows = document.getElementById(tableID).rows.length;
+    // Insert a row at the end of the table
+    let newRow = tableRef.insertRow(rows-1);
+    console.log(rows);
+    let cols = document.getElementById(tableID).rows[0].cells.length
+    for (let i = 0; i < cols-1; i++) {
+        let newCell = newRow.insertCell(i);
+        // let newText = document.createTextNode('(' + rows + ', ' + i + ')');
+        // newCell.appendChild(newText);
+        let cellName = 'cell[' + rows +'][' + Number(i+1) + ']';
+        let cellInput = document.createElement("input");
+        cellInput.setAttribute('class', 'form-control');
+        cellInput.setAttribute('name', cellName);
+        cellInput.setAttribute('placeholder', cellName);
+        newCell.appendChild(cellInput);
+    }
+};
+
+function addCol(tableID) {
+    let tableRef = document.getElementById(tableID);
+    let rows = document.getElementById(tableID).rows.length;
+    console.log(rows);
+    let cols = document.getElementById(tableID).rows[0].cells.length;
+    for (let i = 0; i < rows; i++) {
+        let row = document.getElementById(tableID).rows[i];
+        let newCell = row.insertCell(cols-1);
+        let cellName = 'cell[' + Number(i+1) +'][' + cols + ']';
+        let cellInput = document.createElement("input");
+        cellInput.setAttribute('class', 'form-control');
+        cellInput.setAttribute('name', cellName);
+        if (i==0){
+            cellInput.setAttribute('placeholder','Side ' + cols + ' Name')
+        } else {
+            cellInput.setAttribute('placeholder', cellName);
+        }
+        newCell.appendChild(cellInput);
+    }
+};
