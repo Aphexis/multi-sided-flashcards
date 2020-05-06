e0a1f7e179bc26e8a0cc354365a800d4e986ee78
Aphexis <stephxca@gmail.com>
2020-05-05 21:13:17 -0400

    postgres db migration

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     # FLASK_DEBUG = True
     # DEVELOPMENT = True
     SECRET_KEY = 'temporary'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
