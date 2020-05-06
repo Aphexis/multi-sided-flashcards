79428996a8f5ffb054436c16dd1b87f21bdb1987
Aphexis <stephxca@gmail.com>
2020-05-05 20:38:57 -0400

    postgres db

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config:
-    FLASK_DEBUG = True
-    DEVELOPMENT = True
+    # FLASK_DEBUG = True
+    # DEVELOPMENT = True
     SECRET_KEY = 'temporary'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + os.environ.get('password') + '@localhost/flashcards'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
