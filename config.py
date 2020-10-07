1b1842ec524069f3466bb1c2d787030d73628efc
Stephanie Xu <51296329+Aphexis@users.noreply.github.com>
2020-10-07 02:59:03 -0400

    update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-import os
-
-
 class Config:
     # FLASK_DEBUG = True
     # DEVELOPMENT = True
     SECRET_KEY = 'temporary'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:PASSWORD@localhost/flashcards'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = 'postgres://khkqzrytiqfcem:10b16bd169df54e265bbaf400457307f4e237bed7bb779ce647522521214a025@ec2-52-86-73-86.compute-1.amazonaws.com:5432/da0jv9ga6mh287'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
